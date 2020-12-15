4a50580defb9e9cae686b5f5d3c25725799dc814
alceoritter <alceoritter@gmail.com>
2020-12-15 16:13:57 +0700

    update index

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $api_url = $is_production ?
   'https://app.sandbox.midtrans.com/snap/v1/transactions';
 
 
-if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
+if( !strpos($_SERVER['REQUEST_URI'], '') ) {
   http_response_code(404); 
   echo "wrong path, make sure it's `/charge`"; exit();
 }
